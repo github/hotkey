e629c40d1040137c4542ce5f125de356125c46de
Kristján Oddsson <koddsson@gmail.com>
2019-01-30 17:15:48 +0000

    move tests to same file

--- test/test-hotkey.js
+++ test/test-hotkey.js
@@ -5,21 +5,6 @@ function key(key, target) {
   target.dispatchEvent(new KeyboardEvent('keydown', {key}))
 }
 
-async function wait(ms) {
-  return new Promise(function(resolve) {
-    return setTimeout(resolve, ms)
-  })
-}
-
-// Simulate entering a series of keys with `delay` milliseconds in between
-// keystrokes.
-async function keySequence(keys, delay = 10) {
-  for (const nextKey of keys.split(' ')) {
-    document.dispatchEvent(new KeyboardEvent('keydown', {key: nextKey}))
-    await wait(delay)
-  }
-}
-
 describe('hotkey', function() {
   let activated
   const handler = event => {
@@ -48,64 +33,5 @@ describe('hotkey', function() {
       hotkey.uninstall(button)
     }
     document.body.innerHTML = ''
-  })
-
-  describe('hotkey sequence support', function() {
-    it('supports sequences of 2 keys', async function() {
-      setHTML('<a id="link2" href="#" data-hotkey="b c"></a>')
-      await keySequence('b c')
-      assert.deepEqual(activated, ['link2'])
-    })
-
-    it('finds the longest sequence of keys which maps to something', async function() {
-      setHTML('<a id="link2" href="#" data-hotkey="b c"></a>')
-      await keySequence('z b c')
-      assert.deepEqual(activated, ['link2'])
-    })
-
-    it('supports sequences of 3 keys', async function() {
-      setHTML('<a id="link3" href="#" data-hotkey="d e f"></a>')
-      await keySequence('d e f')
-      assert.deepEqual(activated, ['link3'])
-    })
-
-    it('only exact hotkey sequence matches', async function() {
-      setHTML('<a id="exact" href="#" data-hotkey="j k"></a>')
-      await keySequence('j z k')
-      assert.deepEqual(activated, [])
-    })
-  })
-
-  describe('misc', function() {
-    it('sequences time out after 1500 ms', async function() {
-      setHTML(`
-      <a href="#" id="create1" data-hotkey="h i"></a>
-      <a href="#" id="create2" data-hotkey="i"></a>
-        `)
-
-      keySequence('h')
-      await wait(1550)
-      keySequence('i')
-      assert.deepEqual(activated, ['create2'])
-    })
-
-    it('multiple hotkeys for the same element', async function() {
-      setHTML('<a href="#" id="multiple" data-hotkey="l,m n"></a>')
-
-      await keySequence('l')
-      assert.deepEqual(activated, ['multiple'])
-      await keySequence('m n')
-      assert.deepEqual(activated, ['multiple', 'multiple'])
-    })
-
-    it('with duplicate hotkeys, last element registered wins', async function() {
-      setHTML(`
-      <a href="#" id="duplicate1" data-hotkey="c"></a>
-      <a href="#" id="duplicate2" data-hotkey="c"></a>
-        `)
-
-      await keySequence('c')
-      assert.deepEqual(activated, ['duplicate2'])
-    })
   })
 })
--- test/test.js
+++ test/test.js
@@ -13,6 +13,21 @@ const setHTML = html => {
   }
 }
 
+async function wait(ms) {
+  return new Promise(function(resolve) {
+    return setTimeout(resolve, ms)
+  })
+}
+
+// Simulate entering a series of keys with `delay` milliseconds in between
+// keystrokes.
+async function keySequence(keys, delay = 10) {
+  for (const nextKey of keys.split(' ')) {
+    document.dispatchEvent(new KeyboardEvent('keydown', {key: nextKey}))
+    await wait(delay)
+  }
+}
+
 describe('hotkey', function() {
   beforeEach(function() {
     document.addEventListener('click', clickHandler)
@@ -91,6 +106,65 @@ describe('hotkey', function() {
         done()
       })
       document.body.dispatchEvent(new KeyboardEvent('keydown', {key: '1'}))
+    })
+  })
+
+  describe('hotkey sequence support', function() {
+    it('supports sequences of 2 keys', async function() {
+      setHTML('<a id="link2" href="#" data-hotkey="b c"></a>')
+      await keySequence('b c')
+      assert.deepEqual(elementsActivated, ['link2'])
+    })
+
+    it('finds the longest sequence of keys which maps to something', async function() {
+      setHTML('<a id="link2" href="#" data-hotkey="b c"></a>')
+      await keySequence('z b c')
+      assert.deepEqual(elementsActivated, ['link2'])
+    })
+
+    it('supports sequences of 3 keys', async function() {
+      setHTML('<a id="link3" href="#" data-hotkey="d e f"></a>')
+      await keySequence('d e f')
+      assert.deepEqual(elementsActivated, ['link3'])
+    })
+
+    it('only exact hotkey sequence matches', async function() {
+      setHTML('<a id="exact" href="#" data-hotkey="j k"></a>')
+      await keySequence('j z k')
+      assert.deepEqual(elementsActivated, [])
+    })
+  })
+
+  describe('misc', function() {
+    it('sequences time out after 1500 ms', async function() {
+      setHTML(`
+      <a href="#" id="create1" data-hotkey="h i"></a>
+      <a href="#" id="create2" data-hotkey="i"></a>
+        `)
+
+      keySequence('h')
+      await wait(1550)
+      keySequence('i')
+      assert.deepEqual(elementsActivated, ['create2'])
+    })
+
+    it('multiple hotkeys for the same element', async function() {
+      setHTML('<a href="#" id="multiple" data-hotkey="l,m n"></a>')
+
+      await keySequence('l')
+      assert.deepEqual(elementsActivated, ['multiple'])
+      await keySequence('m n')
+      assert.deepEqual(elementsActivated, ['multiple', 'multiple'])
+    })
+
+    it('with duplicate hotkeys, last element registered wins', async function() {
+      setHTML(`
+      <a href="#" id="duplicate1" data-hotkey="c"></a>
+      <a href="#" id="duplicate2" data-hotkey="c"></a>
+        `)
+
+      await keySequence('c')
+      assert.deepEqual(elementsActivated, ['duplicate2'])
     })
   })
 })
