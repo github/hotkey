92e0249ef9691a422ddcaed58396c29982892c64
Kristján Oddsson <koddsson@gmail.com>
2019-02-14 14:36:58 +0000

    make sure checkbox elements are clicked

--- src/utils.js
+++ src/utils.js
@@ -10,15 +10,30 @@ export function isFormField(element: Node): boolean {
   return (
     name === 'select' ||
     name === 'textarea' ||
-    (name === 'input' && type !== 'submit' && type !== 'reset') ||
+    (name === 'input' && type !== 'submit' && type !== 'reset' && type !== 'checkbox') ||
     element.isContentEditable
   )
+}
+
+function isActivableFormField(element: Node): boolean {
+  if (!(element instanceof HTMLElement)) {
+    return false
+  }
+
+  const name = element.nodeName.toLowerCase()
+  const type = (element.getAttribute('type') || '').toLowerCase()
+  return name === 'input' && type === 'checkbox'
 }
 
 export function fireDeterminedAction(el: HTMLElement): void {
   if (isFormField(el)) {
     el.focus()
-  } else if ((el instanceof HTMLAnchorElement && el.href) || el.tagName === 'BUTTON' || el.tagName === 'SUMMARY') {
+  } else if (
+    (el instanceof HTMLAnchorElement && el.href) ||
+    el.tagName === 'BUTTON' ||
+    el.tagName === 'SUMMARY' ||
+    isActivableFormField(el)
+  ) {
     el.click()
   }
 }
