b14ab35462c78fb645f42f0c85ae0f4810e53aff
Ned Schwartz <ned@theinterned.net>
2021-12-31 17:12:37 -0500

    normalizeHotkey sorts modifiers in consistent order

--- test/test-normalize-hotkey.js
+++ test/test-normalize-hotkey.js
@@ -6,10 +6,12 @@ describe('normalizeHotkey', () => {
   })
 
   const tests = [
+    // Base case control tests
     ['a', 'a'],
     ['Control+a', 'Control+a'],
     ['Meta+a', 'Meta+a'],
     ['Control+Meta+a', 'Control+Meta+a'],
+    // Mod should be localized based on platform
     ['Mod+a', 'Control+a', 'win'],
     ['Mod+a', 'Meta+a', 'mac'],
     ['Mod+A', 'Control+A', 'win'],
@@ -17,7 +19,12 @@ describe('normalizeHotkey', () => {
     ['Mod+9', 'Control+9', 'win'],
     ['Mod+9', 'Meta+9', 'mac'],
     ['Mod+)', 'Control+)', 'win'],
-    ['Mod+)', 'Meta+)', 'mac'] // TODO: on a mac upper-case keys are lowercased when Meta is pressed
+    ['Mod+)', 'Meta+)', 'mac'], // TODO: on a mac upper-case keys are lowercased when Meta is pressed
+    ['Mod+Alt+a', 'Control+Alt+a', 'win'],
+    ['Mod+Alt+a', 'Alt+Meta+a', 'mac'],
+    // Modifier sorting
+    ['Shift+Alt+Meta+Control+m', 'Control+Alt+Meta+Shift+m'],
+    ['Shift+Alt+Mod+m', 'Control+Alt+Shift+m', 'win']
   ]
 
   for (const [input, expected, platform = 'any platform'] of tests) {
