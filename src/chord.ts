9373215452a56bf4957e1004caaee3ba9a3eeb0c
Ian Sanders <iansan5653@github.com>
2023-09-08 19:18:56 +0000

    Add chord mapping and reset button

--- src/chord.ts
+++ src/chord.ts
@@ -5,38 +5,38 @@ interface ChordTrackerOptions {
 export default class ChordTracker {
   static readonly CHORD_TIMEOUT = 1500
 
-  #path: readonly string[] = []
-  #timer: number | null = null
-  #onReset
+  private _path: readonly string[] = []
+  private timer: number | null = null
+  private onReset
 
-  constructor({onReset}: ChordTrackerOptions) {
-    this.#onReset = onReset
+  constructor({onReset}: ChordTrackerOptions = {}) {
+    this.onReset = onReset
   }
 
   get path(): readonly string[] {
-    return this.#path
+    return this._path
   }
 
   registerKeypress(hotkey: string): void {
-    this.#path = [...this.#path, hotkey]
+    this._path = [...this._path, hotkey]
     this.startTimer()
   }
 
   reset(): void {
     this.killTimer()
-    this.#path = []
-    this.#onReset?.()
+    this._path = []
+    this.onReset?.()
   }
 
   private killTimer(): void {
-    if (this.#timer != null) {
-      window.clearTimeout(this.#timer)
+    if (this.timer != null) {
+      window.clearTimeout(this.timer)
     }
-    this.#timer = null
+    this.timer = null
   }
 
   private startTimer(): void {
     this.killTimer()
-    this.#timer = window.setTimeout(() => this.reset(), ChordTracker.CHORD_TIMEOUT)
+    this.timer = window.setTimeout(() => this.reset(), ChordTracker.CHORD_TIMEOUT)
   }
 }
