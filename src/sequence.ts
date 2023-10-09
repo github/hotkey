425fa8811c3cc2c19672989ac84371a59b13680f
Ian Sanders <iansan5653@github.com>
2023-10-09 18:53:42 +0000

    Add `NormalizedSequenceString` and `normalizeSequence`

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,10 +1,18 @@
-import {NormalizedHotkeyString, eventToHotkey} from './hotkey'
+import {NormalizedHotkeyString, eventToHotkey, normalizeHotkey} from './hotkey'
 
 interface SequenceTrackerOptions {
   onReset?: () => void
 }
 
 export const SEQUENCE_DELIMITER = ' '
+
+const sequenceBrand = Symbol('sequence')
+
+/**
+ * Sequence of hotkeys, separated by spaces. For example, `Mod+m g`. Obtain one through the `SequenceTracker` class or
+ * by normalizing a string with `normalizeSequence`.
+ */
+export type NormalizedSequenceString = string & {[sequenceBrand]: true}
 
 export default class SequenceTracker {
   static readonly CHORD_TIMEOUT = 1500
@@ -19,6 +27,10 @@ export default class SequenceTracker {
 
   get path(): readonly NormalizedHotkeyString[] {
     return this._path
+  }
+
+  get sequence(): NormalizedSequenceString {
+    return this._path.join(SEQUENCE_DELIMITER) as NormalizedSequenceString
   }
 
   registerKeypress(event: KeyboardEvent): void {
@@ -43,4 +55,11 @@ export default class SequenceTracker {
     this.killTimer()
     this.timer = window.setTimeout(() => this.reset(), SequenceTracker.CHORD_TIMEOUT)
   }
+}
+
+export function normalizeSequence(sequence: string): NormalizedSequenceString {
+  return sequence
+    .split(SEQUENCE_DELIMITER)
+    .map(h => normalizeHotkey(h))
+    .join(SEQUENCE_DELIMITER) as NormalizedSequenceString
 }
